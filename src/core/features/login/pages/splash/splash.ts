cb0412e3bbd67475824d5a57567f524a35787331
muhammad fajar <invasionfajar@gmail.com>
2023-08-14 15:02:07 +0700

    [IMPL] - Splash, Color change

---
+++ src/core/features/login/pages/splash/splash.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "page-core-login-splash",
+  templateUrl: "splash.html",
+  styleUrls: ["splash.scss"],
+})
+export class CoreLoginSplashPage implements OnInit {
+  ngOnInit(): void {}
+}
